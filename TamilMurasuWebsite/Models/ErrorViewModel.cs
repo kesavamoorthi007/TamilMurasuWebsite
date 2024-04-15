bdd0c76b77d8da0b134458b130268e47f3e0dbf3
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2024-04-15 16:17:29 +0530

    Add project files.

---
+++ TamilMurasuWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TamilMurasuWebsite.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
---
+++ TamilMurasuWebsite/Program.cs
@@ -0,0 +1,74 @@
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Http.Features;
+
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        builder.Services.AddSession();
+
+
+        builder.Services.AddControllers();
+        builder.Services.Configure<FormOptions>(x =>
+        {
+            x.ValueCountLimit = int.MaxValue;
+        });
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+
+        app.UseAuthorization();
+        app.UseSession();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
+        app.Run();
+    }
+
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+            webBuilder.UseWebRoot("wwwroot");
+            webBuilder.UseStartup<IStartup>();
+
+        });
+
+    [Obsolete]
+    public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
+    {
+        // Other configurations...
+
+        app.UseStaticFiles(); // Enable static file serving, e.g., for wwwroot folder
+
+        // More configurations...
+    }
+}
