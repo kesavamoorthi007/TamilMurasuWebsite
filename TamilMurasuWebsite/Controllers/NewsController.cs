878df968e39fcb69cef5cee201e1725a657ebbdb
kesavamoorthi007 <kesavanmoorthi70@gmail.com>
2024-04-18 10:27:33 +0530

    Complete

---
+++ TamilMurasuWebsite/Controllers/NewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TamilMurasuWebsite.Controllers
+{
+	public class NewsController : Controller
+	{
+		public IActionResult News()
+		{
+			return View();
+		}
+	}
+}
